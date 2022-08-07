84e4a7e4d10c4095b8006e083d01655f05baaa55
Aayush Chaube <chaubeaayush@gmail.com>
2022-08-08 00:06:34 +0530

    Added User interactions to the image with the help of Flutter TextButton.

--- lib/main.dart
+++ lib/main.dart
@@ -31,14 +31,18 @@ class DicePage extends StatelessWidget {
       child: Row(
         children: [
           Expanded(
-            child: Padding(
-              padding: const EdgeInsets.all(16.0),
+            child: TextButton(
+              onPressed: () {
+                print('Left button got pressed');
+              },
               child: Image.asset('images/dice1.png'),
             ),
           ),
           Expanded(
-            child: Padding(
-              padding: const EdgeInsets.all(16.0),
+            child: TextButton(
+              onPressed: () {
+                print('Right button got pressed');
+              },
               child: Image.asset('images/dice1.png'),
             ),
           ),
